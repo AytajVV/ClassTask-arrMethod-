405d0933c622e41ea68a3d22e07822bfcf8d1c41
Veyisli Aytaj <AytajVV@code.local>
2022-11-07 18:31:37 +0400

    array count method

---
+++ arrayMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arrayMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Names("lorem", "ipsum", "hello", "515"));
+            //Console.WriteLine(Names(new string[]{ "hello", "bye"}));  
+        }
+        static int Names(params string[] arr)
+        {
+            int count = 0;
+            foreach (var item in arr)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
